7c8b63c90906c59705f1fadb31c6ee4c0ec7ed73
Kerry Sainsbury <kerry@fidelma.com>
2021-11-19 16:35:03 +1300

    Added a TODO. The problem with better exception handling is that you find bugs you weren't aware of before.

--- auxmic.ui/controls/RectConverter.cs
+++ auxmic.ui/controls/RectConverter.cs
@@ -12,9 +12,17 @@ namespace auxmic.ui.controls
     {
         public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            double width = (double)values[0];
-            double height = (double)values[1];
-            return new Rect(0, 0, width, height);
+            try
+            {
+                double width = (double)values[0];
+                double height = (double)values[1];
+                return new Rect(0, 0, width, height);
+            }
+            catch (Exception)
+            {
+                return new Rect(0, 0, 10, 10);  // TODO: Fix properly.
+            }
+           
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
