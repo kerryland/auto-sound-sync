80136c8f521dd3c31cc6b31d7fe6e6ce56085b99
Kerry Sainsbury <kerry@fidelma.com>
2024-04-30 16:37:50 +1200

    Added ffmpeg to build artifact

--- auxmic.ui/Properties/Settings.Designer.cs
+++ auxmic.ui/Properties/Settings.Designer.cs
@@ -12,9 +12,7 @@ namespace auxmic.ui.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
-//    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -27,7 +25,7 @@ namespace auxmic.ui.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute(".\\ffmpeg.exe")]
         public string FFMPEG_EXE_PATH {
             get {
                 return ((string)(this["FFMPEG_EXE_PATH"]));
