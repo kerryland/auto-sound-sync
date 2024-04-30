24f556ca195b00fe742bc632bf406f2438f49529
Kerry Sainsbury <kerry@fidelma.com>
2024-04-30 15:13:31 +1200

    Updated version information

--- auxmic.ui/Properties/AssemblyInfo.cs
+++ auxmic.ui/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("auxmic")]
 [assembly: AssemblyProduct("auxmic")]
-[assembly: AssemblyCopyright("Copyright © 2014-2020 Vladislav Osmanov. 2021-2023 K Sainsbury")]
+[assembly: AssemblyCopyright("Copyright © 2014-2020 Vladislav Osmanov. 2021-2024 K Sainsbury")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -52,6 +52,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.2.0.1")]
+[assembly: AssemblyFileVersion("1.2.0.1")]
 //[assembly: AssemblyInformationalVersion("Build date: 2017-01-26 21:02:40; Revision date: 2017-01-21 16:04:51; Revision(s) in working copy: 95:97; WARNING working copy had uncommitted modifications.")]
